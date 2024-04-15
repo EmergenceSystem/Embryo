99587149d3bef948f8d4051cd925b143e2f6e4ae
Steve Roques <steve.roques@gmail.com>
2024-04-15 17:15:06 +0200

    using map

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
+use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Embryo {
-    pub properties: Vec<EmPair>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct EmPair {
-    pub name: String,
-    pub value: String,
+    pub properties: HashMap<String, String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
